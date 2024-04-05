ed7d33f371beb32a0ed5cff85b228164619b2ec5
Gagan Deep Singh <35302402+gagandeepsingh97@users.noreply.github.com>
2024-04-05 19:46:18 -0400

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { Applicaction } from "./app";
-import { connectToMysql } from "./database"; // Import the MySQL connection function
+import { connectToMySQL } from "./database"; // Import the MySQL connection function
 
 async function main() {
   await connectToMySQL(); // Call the MySQL connection function
