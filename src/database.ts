24dfd9de560a037360434b373df03cbfe7c10afc
Gagan Deep Singh <35302402+gagandeepsingh97@users.noreply.github.com>
2024-04-05 19:45:06 -0400

    Update database.ts

--- src/database.ts
+++ src/database.ts
@@ -1,28 +1,3 @@
-// import { connect, connection } from "mongoose";
-// import { MONGODB_URI } from "./config";
-
-// export async function connectToMongodb() {
-//   try {
-//     await connect(MONGODB_URI);
-//   } catch (error) {
-//     console.log("Error:", error);
-//   }
-// }
-
-// connection.on("connected", () => {
-//   console.log("Mongodb connected to:", connection.db.databaseName);
-// });
-
-// connection.on("error", (error) => {
-//   console.error("error", error);
-// });
-
-// connection.on("disconnected", () => {
-//   console.log("Mongodb disconnected");
-// });
-
-
-
 import mysql from "mysql";
 import { MYSQL_HOST, MYSQL_PORT, MYSQL_DATABASE, MYSQL_USER, MYSQL_PASSWORD } from "./config";
 
