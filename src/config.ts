a5a3b88d0ca61ad12bf8a11e418cb752835e97b4
Gagan Deep Singh <35302402+gagandeepsingh97@users.noreply.github.com>
2024-04-05 20:29:32 -0400

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -2,7 +2,6 @@ import { config } from "dotenv";
 config();
 
 // export const MONGODB_URI = "mongodb://admin:admin@mongodb:27017/tododb";
-// export const PORT = 3000;
 
 export const MYSQL_HOST = "34.136.193.238";
 export const MYSQL_PORT = 3306;
