795f48da232b0ce4ed635e9b8261e896a0756da0
Gagan Deep Singh <35302402+gagandeepsingh97@users.noreply.github.com>
2024-04-06 12:56:28 -0400

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -7,5 +7,5 @@ export const MYSQL_HOST = "34.136.193.238";
 export const MYSQL_PORT = 3306;
 export const MYSQL_DATABASE = "mysqldb";
 export const MYSQL_USER = "admin";
-export const MYSQL_PASSWORD = "admin";
+export const MYSQL_PASSWORD = "admin1";
 export const PORT = 3000;
